abff9de3e0f365b1cb165964261959456f8f9f8c
AngusONeilDunne <angus.ond@gmail.com>
2023-07-01 15:38:30 -0400

    Kinda third level
    Stuff

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -5,11 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
 {
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            NextLevel();
         }
+    }
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
